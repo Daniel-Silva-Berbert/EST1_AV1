b42360fc28c7d82f2e9139aa492db330186c424b
Daniel-Silva-Berbert <139262096+Daniel-Silva-Berbert@users.noreply.github.com>
2024-10-23 07:42:23 -0300

    AV1_EX2
    2. Implemente um algoritmo recursivo para calcular 2n ;

---
+++ src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println(Calculadora.N2(4));
+    }
+}
---
+++ src/Calculadora.java
@@ -0,0 +1,23 @@
+public class Calculadora {
+    public static long N2(int n){
+        if(n == 0)
+            return 1;
+        else{ 
+            if(n == 1)
+                return n;
+            else
+                return N2(n-1, 2);
+        }
+    }
+
+    private static long N2(int n, int total){
+        if(n == 0)
+            return total;
+        else{ 
+            if(n == 1)
+                return total;
+            else
+                return N2(n-1, total * 2);
+        }
+    }
+}
