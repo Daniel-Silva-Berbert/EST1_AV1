0f11396fab8b6094af952cc1849e937b2922effa
Daniel-Silva-Berbert <139262096+Daniel-Silva-Berbert@users.noreply.github.com>
2024-10-24 09:04:07 -0300

    divisão por zero

--- src/MVC.java
+++ src/MVC.java
@@ -1,7 +1,7 @@
 public class MVC {
     public static long mdc(int x, int y){
         if(x == 0 || y == 0){
-            return -1;
+            throw new IllegalArgumentException("O número não pode ser zero.");
         }else{
             if(x >= y && x % y == 0){
                 return y;
