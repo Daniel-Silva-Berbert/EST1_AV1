92b517eb5aaf4e4c7a6c3821da5fe7e73a5bdb95
Daniel-Silva-Berbert <magnoxsbr@gmail.com>
2024-10-23 21:56:36 -0300

    feat #Interação: interação com usúario

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("");
+        Scanner input = new Scanner (System.in);
+        System.out.println("##-------Vamos multiplicar------##");
+        System.out.print("Digite um número inteiro: ");
+
+        try {
+            int num1 = input.nextInt();
+            System.out.print("Digite o segundo número inteiro positivo: ");
+            int num2 = input.nextInt();
+            long resultado = MVC.mdc(num1, num2);
+            System.out.println("O maior multiplico entre " + num1 + " e " + num2 + " é " + resultado);
+        } catch (InputMismatchException im) {
+            System.out.println("\nPor favor, insira um número inteiro positivo. Programa encerrado");
+        }
+        input.close();
     }
 }
