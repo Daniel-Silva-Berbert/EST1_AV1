efacff151e59bc423b33ecb2d11feae36fe3c070
Daniel-Silva-Berbert <magnoxsbr@gmail.com>
2024-10-23 21:45:23 -0300

    feat #Interação: interação com usúario

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(Primo.isPrimo(97));
+        Scanner input = new Scanner (System.in);
+        System.out.println("##-------Vamos Verificar se um número e primo------##");
+        System.out.print("Digite um número inteiro positivo: ");
+
+        try {
+            int numero = input.nextInt();
+            boolean resultado = Primo.isPrimo(numero);
+            System.out.println("O número " + numero + " é " + (resultado? "primo": "não é primo"));
+        } catch (InputMismatchException im) {
+            System.out.println("\nPor favor, insira um número inteiro positivo. Programa encerrado");
+        }
+        catch(IllegalArgumentException ilA){
+            System.out.println("\n" + ilA.getMessage() +". Programa encerrado");
+        }
+        input.close();
+        
     }
 }
--- src/Primo.java
+++ src/Primo.java
@@ -1,14 +1,18 @@
 public class Primo {
     public static boolean isPrimo(int n){
-        if(n < 1){
-            return false;
-        }
-        else{
-            return isPrimo(n, n - 1);
+        if(n < 0){
+            throw new IllegalArgumentException("O número não pode ser negativo.");
+        }else{
+            if(n == 1){
+                return false;
+            }
+            else{
+                return isPrimo(n, n - 1);
+            }
         }
     }
 
-    public static boolean isPrimo(int n, int n2){
+    private static boolean isPrimo(int n, int n2){
         if(n2 == 1)
             return true;
         else{
