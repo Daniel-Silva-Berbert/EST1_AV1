bc21d418114dd9f87a817a21febfb31e4c0b2854
Daniel-Silva-Berbert <magnoxsbr@gmail.com>
2024-10-23 21:39:15 -0300

    feat #Interação: interação com usúario

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(Calculadora.N2(4));
+        Scanner input = new Scanner (System.in);
+        System.out.println("##-------Vamos calcular 2^n------##");
+        System.out.print("Digite um número inteiro positivo: ");
+
+        try {
+            int numero = input.nextInt();
+            long resultado = Calculadora.N2(numero);
+            System.out.println("O resultado de 2^" +numero + " é " + resultado);
+        } catch (InputMismatchException im) {
+            System.out.println("\nPor favor, insira um número inteiro positivo. Programa encerrado");
+        }
+        catch(IllegalArgumentException ilA){
+            System.out.println("\n" + ilA.getMessage() +". Programa encerrado");
+        }
+        input.close();
+        
     }
 }
--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,23 +1,26 @@
 public class Calculadora {
     public static long N2(int n){
-        if(n == 0)
-            return 1;
-        else{ 
-            if(n == 1)
-                return n;
-            else
-                return N2(n-1, 2);
+        if(n < 0)
+        {
+            throw new IllegalArgumentException("O número não pode ser negativo.");
+        }
+        else{
+            if(n == 0)
+                return 1;
+            else{ 
+                if(n == 1)
+                    return n;
+                else
+                    return N2(n-1, 2);
+            }
         }
     }
 
     private static long N2(int n, int total){
         if(n == 0)
             return total;
-        else{ 
-            if(n == 1)
-                return total;
-            else
-                return N2(n-1, total * 2);
+        else{
+            return N2(n-1, total * 2);
         }
     }
 }
