06aaa0b5aade0d9a7c6106f0b2d5657c6a358e51
Daniel-Silva-Berbert <139262096+Daniel-Silva-Berbert@users.noreply.github.com>
2024-10-24 08:53:34 -0300

    feat #EX4: Multiplicações negativas

--- src/SomarMulti.java
+++ src/SomarMulti.java
@@ -1,17 +1,14 @@
 public class SomarMulti {
     public static long somarMulti(int x, int y)
     {
-        if(y < 0)
-        {
-            throw new IllegalArgumentException("O número não pode ser negativo.");
+        if(y == 1){
+            return x;
+        }else if(y < 0){
+            y * -1;
+            return (x + somarMulti(x, y-1))*-1;
         }
         else{
-            if(y == 1){
-                return x;
-            }
-            else{
-                return x + somarMulti(x, y-1);
-            }
+            return x + somarMulti(x, y-1);
         }
     }
 }
